80371c8f608d89035f4ff4d9e43bd86ac6959d05
Julia <ibralenko6724809@gmail.com>
2023-08-10 20:46:41 +0300

    delete app

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import { getFilms } from "./app";
 
 const IMAGES_URL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/6/images?type=POSTER'
+const FILMS_URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films`;
+const API_KEY = `23fa5bf8-77b1-4e9d-8fe5-5040e6c7d436`;
+export async function getFilms (){
+    try {
+        const response = await fetch(FILMS_URL,
+        {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': API_KEY,
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await response.json();
+        const films = await data.items;
+        console.log(films)
+    }
+    catch (err) {
+        console.log(err)
+    }
+};
+
 
 getFilms();
