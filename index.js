90c97341b300d59eca93babfbc9a0e76ff487078
Julia <ibralenko6724809@gmail.com>
2023-08-10 21:23:29 +0300

    add func getPosters

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { getFilms } from "./app";
 
-const IMAGES_URL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/6/images?type=POSTER'
 const FILMS_URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films`;
 const API_KEY = `23fa5bf8-77b1-4e9d-8fe5-5040e6c7d436`;
-export async function getFilms (){
+async function getFilms (){
     try {
         const response = await fetch(FILMS_URL,
         {
@@ -22,5 +21,24 @@ export async function getFilms (){
     }
 };
 
+
+async function getPosters(id){
+        try {
+            const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}/images?type=POSTER`L,
+            {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+            const data = await response.json();
+        }
+        catch (error) {
+            console.error(error)
+        }
+    };
+
+getPosters(Math.floor(Math.random() * 10))
 
 getFilms();
