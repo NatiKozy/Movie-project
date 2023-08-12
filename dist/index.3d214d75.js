eba56a3006a8c82bbce0458af2b78706fcbdae96
Анна Жжонова <annzhzho@gmail.com>
2023-08-12 16:05:08 +0400

    add cards with new API

--- dist/index.3d214d75.js
+++ dist/index.3d214d75.js
@@ -574,8 +574,8 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"bB7Pu":[function(require,module,exports) {
-const FILMS_URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films`;
-const API_KEY = `33b36424-4fa5-41fd-9692-01649a0c6a2c`;
+const FILMS_URL = `https://api.kinopoisk.dev/v1.3/movie`;
+const API_KEY = `V9WW64N-0ZMMV8V-PR39C4M-6YSG9KB`;
 //получить фильмы для карточек MOVIES
 const movieslist = document.querySelector(".movies-list");
 async function getFilms() {
@@ -588,7 +588,7 @@ async function getFilms() {
             }
         });
         const data = await response.json();
-        const films = await data.items;
+        const films = await data.docs;
         console.log(films);
         showMovies(movieslist, films);
     } catch (err) {
@@ -596,18 +596,40 @@ async function getFilms() {
     }
 }
 getFilms();
-function createMovieCard(parrent, link, image, alt) {
+function createMovieCard(parrent, image, alt, year, country, text) {
     const item = document.createElement("li");
-    item.innerHTML = `<a class="link movies-list__link" href="${link}"><img class="movies-list__img" src="${image}" alt="${alt}"></a>`;
+    item.addEventListener("click", (event)=>{
+        event.preventDefault;
+        showMovieModalWindow(image, alt, year, country, text);
+    });
+    item.innerHTML = `<img class="movies-list__img" src="${image}" alt="${alt}">`;
     parrent.append(item);
 }
 function showMovies(parrent, array) {
     for (let item of array){
-        let itemImage = item.posterUrl;
-        let itemLink = `href="#"`;
-        let itemAlt = item.nameRu;
-        createMovieCard(parrent, itemLink, itemImage, itemAlt);
+        const itemImage = item.poster.previewUrl;
+        const itemAlt = item.name;
+        const itemYear = item.year;
+        const itemCountry = item.countries;
+        const itemText = item.description;
+        createMovieCard(parrent, itemImage, itemAlt, itemYear, itemCountry, itemText);
     }
+}
+const modalWindowContainer = document.querySelector(".modal-window__container");
+function showMovieModalWindow(image, alt, year, country, text) {
+    modalWindowContainer.classList.add("modal-window__container--active");
+    const containerStructure = `
+    <div class="modal-window__img">
+    <img src="${image}">
+</div>
+<div class="modal-window__description">
+    <h3 class="modal-window__title">${alt}</h2>
+    <p class="modal-window__year">${year}</p>
+    <p class="modal-window__country">${country}</p>
+    <p class="modal-window__text">${text}</p>
+</div>
+`;
+    modalWindowContainer.innerHTML = containerStructure;
 }
 
 },{}]},["2UeK4","bB7Pu"], "bB7Pu", "parcelRequire3994")
