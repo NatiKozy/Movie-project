a80884ad66de89e217378586967d261929543a05
Julia <ibralenko6724809@gmail.com>
2023-08-12 22:11:49 +0300

    delete getPosters and use prettier

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const HORRIRS_URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=1
 const RANDOM_API_KEY = `23fa5bf8-77b1-4e9d-8fe5-5040e6c7d436`;
 
 
-async function getFilms (){
+async function getFilms() {
     try {
         const response = await fetch(FILMS_URL,
-        {
-            method: 'GET',
-            headers: {
-                'X-API-KEY': API_KEY,
-                'Content-Type': 'application/json',
-            },
-        })
+            {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
         const data = await response.json();
         const films = await data.items;
         console.log(films)
@@ -29,30 +29,11 @@ async function getFilms (){
 };
 
 
-async function getPosters(id){
-        try {
-            const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}/images?type=POSTER`,
-            {
-                method: 'GET',
-                headers: {
-                    'X-API-KEY': API_KEY,
-                    'Content-Type': 'application/json',
-                },
-            })
-            const data = await response.json();
-        }
-        catch (error) {
-            console.error(error)
-        }
-    };
-
-getPosters(Math.floor(Math.random() * 10))
-
 getFilms();
 
- async function getPremiers() {
+async function getPremiers() {
     try {
-        const response = await fetch(PREMIERS_URL, 
+        const response = await fetch(PREMIERS_URL,
             {
                 method: 'GET',
                 headers: {
@@ -60,174 +41,173 @@ getFilms();
                     'Content-Type': 'application/json',
                 },
             })
-            const data = await response.json();
-            const premiers = await data.items;
-            console.log(premiers);
-            showPremiers(premiers);
-        }
-        catch (err) {
-            console.log(err)
-        }
+        const data = await response.json();
+        const premiers = await data.items;
+        console.log(premiers);
+        showPremiers(premiers);
     }
+    catch (err) {
+        console.log(err)
+    }
+}
 
 getPremiers();
-    
-    const premiersContainer = document.querySelector('.premiers');
 
-    function showPremiers(array) {
-        for(item of array) {
-            const div = document.createElement('div');
-            premiereImage = item.posterUrl;
-            div.innerHTML = `
+const premiersContainer = document.querySelector('.premiers');
+
+function showPremiers(array) {
+    for (item of array) {
+        const div = document.createElement('div');
+        premiereImage = item.posterUrl;
+        div.innerHTML = `
             <img src="${premiereImage}">
             `
-            premiersContainer.append(div);
+        premiersContainer.append(div);
 
-        }
     }
+}
 
-    function arrayRandElement(arr) {
-        const rand = Math.floor(Math.random() * arr.length);
-        return arr[rand]
+function arrayRandElement(arr) {
+    const rand = Math.floor(Math.random() * arr.length);
+    return arr[rand]
+}
+
+async function getRandomDramaMovie() {
+    try {
+        const response = await fetch(DRAMAS_URL,
+            {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': RANDOM_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+        const data = await response.json();
+        const res = arrayRandElement(data.items)
+        console.log(res);
+        showRandomMovie(res)
     }
-    
-    async function getRandomDramaMovie() {
-        try {
-            const response = await fetch(DRAMAS_URL,
-                {
-                    method: 'GET',
-                    headers: {
-                        'X-API-KEY': RANDOM_API_KEY,
-                        'Content-Type': 'application/json',
-                    },
-                })
-            const data = await response.json();
-            const res = arrayRandElement(data.items)
-            console.log(res);
-            showRandomMovie(res)
-        }
-        catch (error) {
-            console.error(error)
-        }
-    };
-    
-    
-    
-    
-    async function getRandomHorrorMovie() {
-        try {
-            const response = await fetch(HORRIRS_URL,
-                {
-                    method: 'GET',
-                    headers: {
-                        'X-API-KEY': RANDOM_API_KEY,
-                        'Content-Type': 'application/json',
-                    },
-                })
-            const data = await response.json();
-            const res = arrayRandElement(data.items)
-            console.log(res);
-            showRandomMovie(res)
-        }
-        catch (error) {
-            console.error(error)
-        }
-    };
-    
-    async function getRandomComedyMovie() {
-        try {
-            const response = await fetch(COMEDIES_URL,
-                {
-                    method: 'GET',
-                    headers: {
-                        'X-API-KEY': RANDOM_API_KEY,
-                        'Content-Type': 'application/json',
-                    },
-                })
-            const data = await response.json();
-            const res = arrayRandElement(data.items)
-            console.log(res);
-            showRandomMovie(res)
-        }
-        catch (error) {
-            console.error(error)
-        }
-    };
-    
-    async function getRandomFantasyMovie() {
-        try {
-            const response = await fetch(FANTASY_URL,
-                {
-                    method: 'GET',
-                    headers: {
-                        'X-API-KEY': RANDOM_API_KEY,
-                        'Content-Type': 'application/json',
-                    },
-                })
-            const data = await response.json();
-            const res = arrayRandElement(data.items)
-            console.log(res);
-            showRandomMovie(res)
-        }
-        catch (error) {
-            console.error(error)
-        }
-    };
-    
-    async function getRandomTrillesMovie() {
-        try {
-            const response = await fetch(TRILLERS_URL,
-                {
-                    method: 'GET',
-                    headers: {
-                        'X-API-KEY': RANDOM_API_KEY,
-                        'Content-Type': 'application/json',
-                    },
-                })
-            const data = await response.json();
-            const res = arrayRandElement(data.items)
-            console.log(res);
-            showRandomMovie(res)
-        }
-        catch (error) {
-            console.error(error)
-        }
-    };
-    
-    const randomMovie = document.querySelector('.random-movie')
-    function showRandomMovie(movie) {
-        const topRandomMovie = document.querySelector('div')
-        topRandomMovie.innerHTML = `<h2 class="random-movie-title">${movie.nameRu}</h2>
+    catch (error) {
+        console.error(error)
+    }
+};
+
+
+
+
+async function getRandomHorrorMovie() {
+    try {
+        const response = await fetch(HORRIRS_URL,
+            {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': RANDOM_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+        const data = await response.json();
+        const res = arrayRandElement(data.items)
+        console.log(res);
+        showRandomMovie(res)
+    }
+    catch (error) {
+        console.error(error)
+    }
+};
+
+async function getRandomComedyMovie() {
+    try {
+        const response = await fetch(COMEDIES_URL,
+            {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': RANDOM_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+        const data = await response.json();
+        const res = arrayRandElement(data.items)
+        console.log(res);
+        showRandomMovie(res)
+    }
+    catch (error) {
+        console.error(error)
+    }
+};
+
+async function getRandomFantasyMovie() {
+    try {
+        const response = await fetch(FANTASY_URL,
+            {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': RANDOM_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+        const data = await response.json();
+        const res = arrayRandElement(data.items)
+        console.log(res);
+        showRandomMovie(res)
+    }
+    catch (error) {
+        console.error(error)
+    }
+};
+
+async function getRandomTrillesMovie() {
+    try {
+        const response = await fetch(TRILLERS_URL,
+            {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': RANDOM_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+        const data = await response.json();
+        const res = arrayRandElement(data.items)
+        console.log(res);
+        showRandomMovie(res)
+    }
+    catch (error) {
+        console.error(error)
+    }
+};
+
+const randomMovie = document.querySelector('.random-movie')
+function showRandomMovie(movie) {
+    const topRandomMovie = document.querySelector('div')
+    topRandomMovie.innerHTML = `<h2 class="random-movie-title">${movie.nameRu}</h2>
     <img class="random-movie-img" src="${movie.posterUrl}">
     <div class="movie__category">${movie.genres.map(
-            (genre) => ` ${genre.genre}`
-        )}</div>`
+        (genre) => ` ${genre.genre}`
+    )}</div>`
+}
+
+
+function checkSelect() {
+    const selectedValue = document.getElementById("select-list").value;
+
+    if (selectedValue === "Мелодрама") {
+        getRandomDramaMovie()
+    } else if (selectedValue === "Комедия") {
+        getRandomComedyMovie()
+    } else if (selectedValue === "Ужасы") {
+        getRandomHorrorMovie()
+    } else if (selectedValue === "Триллер") {
+        getRandomTrillesMovie()
     }
-    
-    
-    function checkSelect() {
-        const selectedValue = document.getElementById("select-list").value;
-    
-        if (selectedValue === "Мелодрама") {
-            getRandomDramaMovie()
-        } else if (selectedValue === "Комедия") {
-            getRandomComedyMovie()
-        } else if (selectedValue === "Ужасы") {
-            getRandomHorrorMovie()
-        } else if (selectedValue === "Триллер") {
-            getRandomTrillesMovie()
-        }
-    }
-    
-    
-    const form = document.querySelector('.random-form')
-    
-    form.addEventListener('change', (e) => {
-        e.preventDefault();
-    
-        checkSelect()
-    });
-    
-    
-    
-    
+}
+
+
+const form = document.querySelector('.random-form')
+
+form.addEventListener('change', (e) => {
+    e.preventDefault();
+
+    checkSelect()
+});
+
+
+
