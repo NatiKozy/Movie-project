0fecbe1cfc5d880942a4856f4ddab007e16e7d39
Elena <knopka-el@ya.ru>
2023-08-20 15:48:54 +0200

    modal window

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function checmoviekType(array) {
         if (item.type === "movie") {
             const movies = [];
             movies.push(item);
-            showMoviesCards(movieslist, movies)
+            showMoviesCards(movieslist, array)
         } else if (item.type === "tv-series") {
             const series = [];
             series.push(item);
@@ -338,7 +338,6 @@ async function displayMoviesInModal() {
               <div class="search-movie__info">
                 <div class="search-movie__title">${movieData.nameRu}</div>
                 <div class="search-movie__category">${movieData.genres.map(genre => ` ${genre.genre}`)}</div>
-                <div class="search.movie__average ">${movieData.rating}</div>
               </div>`;
         modalMoviesContainer.appendChild(movieEl);
     });
